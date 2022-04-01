10c5ba7e649730fb0606ff7d008ec59b8b701820
Ray Hawthorne <rayhawthorne@W056902-MB.local>
2022-04-01 12:51:02 -0400

    Initial commit of Jazz Playlist

---
+++ wsmr/jazzPlaylist.js
@@ -0,0 +1,46 @@
+'use strict'
+
+function JazzPlaylist() {
+    const [ playlist, setPlaylist ] = React.useState([])
+
+    React.useEffect(() => {
+        async function fetchPlaylist() {
+           let response = await fetch('jazzPlaylist.json')
+           response = await response.json()
+           setPlaylist(response.reverse())
+        } 
+        fetchPlaylist()
+    }, [])
+
+    return (
+        <div className='playlist__app-container'>
+            <div className='playlist__title'>
+                Jazz Listings for {moment().format('dddd, MMMM Do, YYYY')}
+            </div>
+            <table className='table align-middle'>
+                <thead>
+                    <tr className='playlist__headers'>
+                        <th><i className="fa-solid fa-clock"></i></th>
+                        <th>Artist</th>
+                        <th>Title</th>
+                        <th>Label</th>
+                        <th>Composer</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {playlist.map((song, rowNumber) => {
+                    return (
+                        <tr key={song.id} style={{ backgroundColor: rowNumber % 2 == 0 ? '#F2F2F1' : 'white' }}>
+                            <td className='playlist__time'>{moment(Date.parse(song.extraInfo.AirStarttime)).format('LT')}</td>
+                            <td dangerouslySetInnerHTML={{__html: song.extraInfo.artist}}></td>
+                            <td dangerouslySetInnerHTML={{__html: song.extraInfo.title}}></td>
+                            <td dangerouslySetInnerHTML={{__html: song.extraInfo.Label}}></td>
+                            <td dangerouslySetInnerHTML={{__html: song.extraInfo.Composer}}></td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
