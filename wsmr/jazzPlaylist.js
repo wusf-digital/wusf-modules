cce4e86617adbd6f7e68ec5f116808f273e34f90
Ray Hawthorne <rayhawthorne@W056902-MB.lan>
2022-04-03 18:16:37 -0400

    Enable users to view playlists by day using previous and next buttons

--- wsmr/jazzPlaylist.js
+++ wsmr/jazzPlaylist.js
@@ -4,18 +4,22 @@ function JazzPlaylist() {
     const [ playlist, setPlaylist ] = React.useState([])
     const [ date, setDate ] = React.useState(moment().format('YYYY-MM-DD'))
     const [ displayDate, setDisplayDate ] = React.useState('')
+    const [ disabled, setDisabled ] = React.useState(true)
 
     React.useEffect(() => {
         async function fetchPlaylist(date) {
             try {
-                // let response = await fetch(`https://api.wusf.org/v2/songs/WUSF/day?date=${date}`)
+                //let response = await fetch(`https://api.wusf.org/v2/songs/WUSF/day?date=${date}`)
                 let response = await fetch(`jazzPlaylist_2022-03-31.json`)
                 response = await response.json()
 
                 // Reverse the playlist array so the latest is displayed first
                 setPlaylist(response.reverse())
 
-                // Check to see if there are song data for current date. If not, subtract one day
+                /* 
+                Check to see if there are song data for current date. If not, subtract one day,
+                and store the data.
+                */
                 if (Array.isArray(response) && !response.length) {
                     setDate(moment().subtract(1, 'days').format('YYYY-MM-DD'))
                 }
@@ -23,26 +27,42 @@ function JazzPlaylist() {
                 // Set the date displayed to user based on first song returned from the server
                 if (typeof response !== undefined && response[0]) {
                     const serverDate = response[0].start.dateTime
-                    setDisplayDate(moment(serverDate).format('dddd, MMMM Do, YYYY'))
+                    setDisplayDate(serverDate)
                 }
+
+                /* 
+                If the date displayed to user is at least one day old,
+                change the 'Next' button to be enabled. Otherwise, leave it disabled.
+                */
+                moment().diff(displayDate, 'd') >= 1 ? setDisabled(false) : setDisabled(true)
                 
             } catch (error) {
                 console.error(error)
             }
-            
         }
         fetchPlaylist(date)
-    }, [ date ])
-
+    }, [ date, displayDate ])
     
+
+    function previousDate() {
+        setDate(moment(date).subtract(1, 'days').format('YYYY-MM-DD'))
+    }
+
+    function nextDate() {
+        setDate(moment(date).add(1, 'days').format('YYYY-MM-DD'))
+    }
 
     return (
         <div className='playlist__app-container'>
             { displayDate && 
                 <p className='playlist__title'>
-                    Jazz Listings for {displayDate}
+                    Jazz Listings for {moment(displayDate).format('dddd, MMMM Do, YYYY')}
                 </p>
             }
+            <section className='playlist__button-group'>
+                <button className='playlist__button--previous' onClick={previousDate}>Previous</button>
+                <button className='playlist__button--next' disabled={disabled} onClick={nextDate}>Next</button>
+            </section>
             <table className='table align-middle'>
                 <thead>
                     <tr className='playlist__headers'>
