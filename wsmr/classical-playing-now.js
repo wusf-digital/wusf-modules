40331652c31a320d3d55e2f5aad78515facc8246
Ray Hawthorne <rayhawthorne@W056902-MB.lan>
2022-06-06 13:43:10 -0400

    Break out playing now JS into its own file

---
+++ wsmr/classical-playing-now.js
@@ -0,0 +1,14 @@
+const playingNowSummaryExists = !!document.querySelector('#playing-now__summary')
+
+async function currentlyPlaying() {
+    try {
+        let response = await (await fetch(`https://api.wusf.org/v2/songs/WSMR/recent`)).json()
+        response = response.reverse()[0]
+        if (playingNowSummaryExists) {
+        	document.querySelector('#playing-now__summary').innerHTML = response.summary
+        }
+    } catch(error) {
+        console.error(error)
+    }
+} 
+currentlyPlaying()
