02d0779c5fae29b045aa58f3f5b3afabc90d5038
Ray Hawthorne <rayhawthorne@W056902-MB.lan>
2022-07-08 15:07:25 -0400

    Remove margins for Classical Playing Now module

--- brands/wsmr/scripts/classical-playing-now.js
+++ brands/wsmr/scripts/classical-playing-now.js
@@ -2,7 +2,7 @@ const playingNowSummaryExists = !!document.querySelector('#playing-now__summary'
 
 async function currentlyPlaying() {
     try {
-        let response = await (await fetch(`https://api.wusf.org/v2/songs/WSMR/recent`)).json()
+        let response = await (await fetch(`https://api.wsmr.org/v2/songs/WSMR/recent`)).json()
         response = response.reverse()[0]
         if (playingNowSummaryExists) {
         	document.querySelector('#playing-now__summary').innerHTML = response.summary
