2ef24c3b6bc805f1814cddc35127f93c509dabec
Ray Hawthorne <rayhawthorne@W056902-MB.local>
2022-08-16 21:05:20 -0400

    Add front end authentication

--- ai/scripts/polly.js
+++ ai/scripts/polly.js
@@ -3,36 +3,83 @@ const form = document.getElementById('polly')
 const voiceType = document.getElementById('select-voice')
 const submitButton = document.getElementById('polly-submit')
 
-function start() {
-    const data = {}
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-        removeAudioPlayer()
-        let textToSend = "<speak><amazon:domain name='news'>"
-        textToSend += document.getElementById('polly-text').value
-        textToSend += "</amazon:domain></speak>"
-        data.data = {}
-        data.data.text = textToSend
-        data.data.voice = voiceType.options[voiceType.selectedIndex].value
-        console.log(JSON.stringify(data))
-        submitButton.disabled = true
-        submitToApi(data)
-    })
+function enterPassword() {
+    return prompt('Enter the password')
 }
 
-async function submitToApi(data) {
+function getToken() {
+    return localStorage.getItem('token')
+}
+
+function setToken(token) {
+    localStorage.setItem('token', token)
+}
+
+async function authenticate() {
+    const password = enterPassword()
+
     try {
-        const response = await fetch('https://api-dev.wusf.digital/polly', {
+        const response = await fetch('http://localhost:3000/user/login', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "email": "digital@wusf.org",
+                password
+            })
+        })
+        
+        if (response.ok) {
+            let token = await response.json()
+            setToken(token.token)
+            collectFormData()
+        } else {
+            enterPassword()
+        }
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+function run() {
+    form.addEventListener('submit', e => {
+        e.preventDefault() 
+        authenticate()
+    })
+}
+
+function collectFormData() {
+    removeAudioPlayer()
+    const data = {}
+    let textToSend = "<speak><amazon:domain name='news'>"
+    textToSend += document.getElementById('polly-text').value
+    textToSend += "</amazon:domain></speak>"
+    data.data = {}
+    data.data.text = textToSend
+    data.data.voice = voiceType.options[voiceType.selectedIndex].value
+    submitButton.disabled = true
+    submitToApi(data)
+}
+
+async function submitToApi(data) {
+    try {
+        const response = await fetch('http://localhost:3000/polly', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${getToken()}`
             },
             body: JSON.stringify(data)
         })
         if (response.ok) {
             const audioStreamURL = await response.json()
             createAudioPlayer(audioStreamURL)
+        } else {
+            alert('An authentication error occurred.')
         }
     } catch(error) {
         console.error(error)
@@ -53,4 +100,4 @@ function removeAudioPlayer() {
     document.querySelector('audio').remove()
 }
 
-start()
+run()
